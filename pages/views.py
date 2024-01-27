02bdf1e0309479157145cf69f0e85da592c5ae12
Akramov Amirbek <125534574+amirbek-akramov@users.noreply.github.com>
2024-01-27 23:27:02 +0500

    Updated

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from posts.models import Post
 
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.order_by('-date')[:10]
